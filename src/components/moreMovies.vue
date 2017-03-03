664b9f28bdc821c14d9ca50ee104631645292669
Yawenina <xiaoywkate@163.com>
2017-03-03 18:24:29 +0800

    update fetchData count

--- src/components/moreMovies.vue
+++ src/components/moreMovies.vue
@@ -3,8 +3,10 @@
     <div class="more">
       <loading-bar ref="loadingBar"></loading-bar>
       <loading v-if="!isDataLoaded"></loading>
+
       <div class="content" v-else @scroll="scrollHandler" ref="mainContent" v-set-height>
         <h2 class="category-title">{{$route.query.category}}</h2>
+
         <div class="more-currentRenderedData">
           <router-link v-for="item in currentRenderedData" class="item" :to="'/movie/subject/' + item.id">
             <img class="item-poster" v-lazy="item.images.large">
@@ -17,7 +19,11 @@
             </div>
           </router-link>
         </div>
+
+        <p class="is-loading loading-tip" v-if="!allDataLoaded">正在加载中...</p>
+        <p class="no-more loading-tip" v-else="allDataLoaded">Oops...没有更多啦</p>
       </div>
+    </div>
 </template>
 
 <script>
@@ -43,10 +49,13 @@
     data() {
       return {
         isDataLoaded: false,
+        allDataCount: 0,
+        allDataLoaded: false,
         currentRenderedData: [],
-        allData:[],
-        allDataLen: 0,
+        currRenderedDataCount: 0,
         busy: false,
+        api: '',
+        isLoadComplete: true,
       };
     },
 //    created() {
@@ -56,31 +65,39 @@
 //        .then(response => (this.currentRenderedData = response.data.subjects.slice(0, 18)));
 //    },
     beforeRouteEnter(to, from, next) {
-      let api = `/v2/movie/${to.params.type}`;
       next((vm) => {
+        vm.api = `/v2/movie/${to.params.type}?count=9`;
         vm.$refs.loadingBar.start();
-        vm.fetchData(api).then((response) => {
-          vm.allData = response.data.subjects;
-          vm.allDataLen = vm.allData.length;
-          vm.currentRenderedData = response.data.subjects.slice(0, 9);
+        vm.fetchData(vm.api).then((response) => {
+          let data = response.data;
+          vm.allDataCount = data.total;
+          vm.updateDataInfo(data.subjects, data.count);
           vm.$refs.loadingBar.finish();
           vm.isDataLoaded = true;
         })
       })
     },
-    computed: {
-      currRenderedDataLen() {
-        return this.currentRenderedData.length;
-      }
-    },
     methods: {
       fetchData(api) {
         return this.$axios.get(api);
       },
+      updateDataInfo(newData, newDataCount) {
+        this.currentRenderedData = this.currentRenderedData.concat(newData);
+        this.currRenderedDataCount = this.currRenderedDataCount + newDataCount >= this.allDataCount ? this.allDataCount : this.currRenderedDataCount + newDataCount;
+        if (this.currRenderedDataCount >= this.allDataCount) {
+          this.allDataLoaded = true;
+        }
+      },
       loadMore() {
-        if (this.currRenderedDataLen >= this.allDataLen) return;
-        this.currentRenderedData = this.currentRenderedData.concat(this.allData.slice(this.currRenderedDataLen, this.currRenderedDataLen + 9));
-        this.currRenderedDataLen += 9;
+        if (this.currRenderedDataCount >= this.allDataCount) return;
+
+        this.isLoadComplete = false;
+        let api = `${this.api}&start=${this.currRenderedDataCount}`;
+        this.fetchData(api).then(response => {
+          let data = response.data;
+          this.updateDataInfo(data.subjects, data.count);
+          this.isLoadComplete = true;
+        })
       },
       scrollHandler() {
         let ref = this.$refs.mainContent;
@@ -88,7 +105,7 @@
             scrollHeight = ref.scrollHeight,
             clientHeight = ref.clientHeight;
 
-        if (scrollHeight - 10 <= scrollTop + clientHeight) {
+        if (scrollHeight - 30 <= scrollTop + clientHeight && this.isLoadComplete) {
           this.loadMore();
         }
       }
@@ -123,5 +140,10 @@
     white-space:nowrap;
     overflow: hidden;
     text-overflow: ellipsis;
+  }
+  .loading-tip{
+    font-size: 1.1em;
+    color: #42bd56;
+    text-align: center;
   }
 </style>
